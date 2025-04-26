1b2a0bb7ca3d06acf96000311c15880dcfe485af
Isara Nakavisute <isara_nakavisute@hotmail.com>
2025-04-26 08:58:17 +0700

    fix code

--- db.js
+++ db.js
@@ -2,10 +2,9 @@ const mysql = require('mysql2/promise');
 const config = require('./config');
 
 async function query(sql, params) {
-  //const connection = await mysql.createConnection(config.db);
   const connection = await mysql.createPool(config.db);
   const [results, ] = await connection.execute(sql, params);
-  await connection.end();
+  connection.end();
   return results;
 }
 
