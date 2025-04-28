2f6014dc58421b57b9260b532508d3d4eafc020e
Isara Nakavisute <isara_nakavisute@hotmail.com>
2025-04-28 15:00:17 +0700

    fix code

--- index.js
+++ index.js
@@ -727,14 +727,22 @@ app.post("/master_data/upload", async (req, res) => {
                     fs.writeFileSync('uploaded_files/temp.txt', 'start'+'\r\n');
                     fs.writeFileSync('uploaded_files/sql.log', 'start'+'\r\n');
 
-                    console.log("total rows="+numberOfRows);
-                    return;
+//                    console.log("total rows="+numberOfRows);
+//                    return;
 
                     rows.map(async (row) => {
 
                           row_cnt++;
                           console.log("row="+row_cnt);
 
+//                          if (row_cnt > numberOfRows)
+//                           break;
+
+                          if (row_cnt <= numberOfRows)
+                          {
+//
+//
+//                          }
                           sql="insert into master_tsubakimoto(category,part_no,previous_model_no,new_model_no,unit,manufacturer_suggested_retail_price,new_manufacturer_suggested_retail_price,conversion_to_ft,diff_for_cost,op_price,po_price_jpy_usd,po_price_currency,remark,thb_cost,gp,pricelist_name,multiplier,make_same_price_as_standard_price,new_make_same_price_as_standard_price,standard_price,diff,dist_pl_mull,dist_ex_rate,unit_price,new_unit_price,diff_unit_price,status,supplier_name,stock_reference,cutting_assembly,detail)";
                           sql += " values ('";
                           value = row.getCell(1).formula;
@@ -1114,6 +1122,9 @@ app.post("/master_data/upload", async (req, res) => {
 
 
                           //await db.query(sql);
+
+                         }
+
                        })
 
                        line_cnt = 0;
